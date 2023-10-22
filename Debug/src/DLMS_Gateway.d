298ba2f0bca02e8f68c6a241d4c185f1e7ee404c
amirhh1379 <amirhosein.hajian19@gmail.com>
2023-10-22 13:19:16 +0330

    321

--- Debug/src/DLMS_Gateway.d
+++ Debug/src/DLMS_Gateway.d
@@ -1,88 +1,87 @@
 src/DLMS_Gateway.o: ../src/DLMS_Gateway.c \
- /home/mhn/DMGProject/ZS-DMG/include/DLMS_Gateway.h \
- /home/mhn/DMGProject/ZS-DMG/include/main.h \
- /home/mhn/DMGProject/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/DSI_ConnectManager.h \
- /home/mhn/DMGProject/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_nw.h \
- /home/mhn/DMGProject/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_sms.h \
- /home/mhn/DMGProject/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_sim.h \
- /home/mhn/DMGProject/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_gpio.h \
- /home/mhn/DMGProject/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_uart.h \
- /home/mhn/DMGProject/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_i2c.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/cosem.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/gxobjects.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/gxignore.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/enums.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/variant.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/date.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/bytebuffer.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/gxint.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/errorcodes.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/bitarray.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/gxarray.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/gxkey.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/gxdefine.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/dlmssettings.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/objectarray.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/message.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/gxvalueeventargs.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/replydata.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/ciphering.h \
- /home/mhn/DMGProject/ZS-DMG/development/include/gxaes.h
+ /home/mhn/ZS-DMG/include/DLMS_Gateway.h /home/mhn/ZS-DMG/include/main.h \
+ /home/mhn/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/DSI_ConnectManager.h \
+ /home/mhn/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_nw.h \
+ /home/mhn/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_sms.h \
+ /home/mhn/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_sim.h \
+ /home/mhn/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_gpio.h \
+ /home/mhn/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_uart.h \
+ /home/mhn/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_i2c.h \
+ /home/mhn/ZS-DMG/development/include/cosem.h \
+ /home/mhn/ZS-DMG/development/include/gxobjects.h \
+ /home/mhn/ZS-DMG/development/include/gxignore.h \
+ /home/mhn/ZS-DMG/development/include/enums.h \
+ /home/mhn/ZS-DMG/development/include/variant.h \
+ /home/mhn/ZS-DMG/development/include/date.h \
+ /home/mhn/ZS-DMG/development/include/bytebuffer.h \
+ /home/mhn/ZS-DMG/development/include/gxint.h \
+ /home/mhn/ZS-DMG/development/include/errorcodes.h \
+ /home/mhn/ZS-DMG/development/include/bitarray.h \
+ /home/mhn/ZS-DMG/development/include/gxarray.h \
+ /home/mhn/ZS-DMG/development/include/gxkey.h \
+ /home/mhn/ZS-DMG/development/include/gxdefine.h \
+ /home/mhn/ZS-DMG/development/include/dlmssettings.h \
+ /home/mhn/ZS-DMG/development/include/objectarray.h \
+ /home/mhn/ZS-DMG/development/include/message.h \
+ /home/mhn/ZS-DMG/development/include/gxvalueeventargs.h \
+ /home/mhn/ZS-DMG/development/include/replydata.h \
+ /home/mhn/ZS-DMG/development/include/ciphering.h \
+ /home/mhn/ZS-DMG/development/include/gxaes.h
 
-/home/mhn/DMGProject/ZS-DMG/include/DLMS_Gateway.h:
+/home/mhn/ZS-DMG/include/DLMS_Gateway.h:
 
-/home/mhn/DMGProject/ZS-DMG/include/main.h:
+/home/mhn/ZS-DMG/include/main.h:
 
-/home/mhn/DMGProject/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/DSI_ConnectManager.h:
+/home/mhn/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/DSI_ConnectManager.h:
 
-/home/mhn/DMGProject/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_nw.h:
+/home/mhn/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_nw.h:
 
-/home/mhn/DMGProject/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_sms.h:
+/home/mhn/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_sms.h:
 
-/home/mhn/DMGProject/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_sim.h:
+/home/mhn/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_sim.h:
 
-/home/mhn/DMGProject/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_gpio.h:
+/home/mhn/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_gpio.h:
 
-/home/mhn/DMGProject/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_uart.h:
+/home/mhn/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_uart.h:
 
-/home/mhn/DMGProject/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_i2c.h:
+/home/mhn/ZS-DMG/DMG-SDK/Include/ql-common-lib/include/ql_i2c.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/cosem.h:
+/home/mhn/ZS-DMG/development/include/cosem.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/gxobjects.h:
+/home/mhn/ZS-DMG/development/include/gxobjects.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/gxignore.h:
+/home/mhn/ZS-DMG/development/include/gxignore.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/enums.h:
+/home/mhn/ZS-DMG/development/include/enums.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/variant.h:
+/home/mhn/ZS-DMG/development/include/variant.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/date.h:
+/home/mhn/ZS-DMG/development/include/date.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/bytebuffer.h:
+/home/mhn/ZS-DMG/development/include/bytebuffer.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/gxint.h:
+/home/mhn/ZS-DMG/development/include/gxint.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/errorcodes.h:
+/home/mhn/ZS-DMG/development/include/errorcodes.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/bitarray.h:
+/home/mhn/ZS-DMG/development/include/bitarray.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/gxarray.h:
+/home/mhn/ZS-DMG/development/include/gxarray.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/gxkey.h:
+/home/mhn/ZS-DMG/development/include/gxkey.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/gxdefine.h:
+/home/mhn/ZS-DMG/development/include/gxdefine.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/dlmssettings.h:
+/home/mhn/ZS-DMG/development/include/dlmssettings.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/objectarray.h:
+/home/mhn/ZS-DMG/development/include/objectarray.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/message.h:
+/home/mhn/ZS-DMG/development/include/message.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/gxvalueeventargs.h:
+/home/mhn/ZS-DMG/development/include/gxvalueeventargs.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/replydata.h:
+/home/mhn/ZS-DMG/development/include/replydata.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/ciphering.h:
+/home/mhn/ZS-DMG/development/include/ciphering.h:
 
-/home/mhn/DMGProject/ZS-DMG/development/include/gxaes.h:
+/home/mhn/ZS-DMG/development/include/gxaes.h:
